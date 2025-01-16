283b7251c62a63b1154294d27498052d7f52b70b
Ulf Dellbrügge <xintamosaik@gmail.com>
2025-01-17 00:49:22 +0100

    refactor tokenize.c to enhance character classification and improve output formatting

--- tokenize.c
+++ tokenize.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <ctype.h>
+
 int main(int argc, char const *argv[])
 {
     const char input[] = "name = \"Ulf\"\npi = 3.14\nlog(name)";
@@ -8,14 +9,35 @@ int main(int argc, char const *argv[])
     int i;
     for (i = 0; i < input_length; i++) {
      
-        printf("\n'%c' (ASCII value %d) is ", input[i], input[i]);
-        if (isalnum(input[i])) printf("alphanumeric ");
-        if (isalpha(input[i])) printf("letter ");
-        if (isblank(input[i])) printf("blank ");
-        if (iscntrl(input[i])) printf("control ");
-        if (isdigit(input[i])) printf("digit ");
-        if (isdigit(input[i])) printf("digit ");
-
+        int code;
+        // if (isalnum(input[i])) printf("alphanumeric ");
+        const int is_alpha = isalpha(input[i]);
+        const int is_blank = isblank(input[i]);
+        const int is_cntrl = iscntrl(input[i]);
+        const int is_digit = isdigit(input[i]);
+        const int is_graph = isgraph(input[i]);
+        const int is_lower = islower(input[i]);
+        const int is_print = isprint(input[i]);
+        const int is_punct = ispunct(input[i]);
+        const int is_space = isspace(input[i]);
+        const int is_upper = isupper(input[i]);
+        if (is_graph) printf("\n'%c' (ASCII value %d) is ", input[i], input[i]);
+        else printf("\n_ _ (ASCII value %d) is ", input[i]);
+        // if (is_lower) printf("lower "); // return LOWER
+        // if (is_upper) printf("upper "); // return UPPER
+        // if (is_alpha) printf("letter ");
+        // if (is_digit) printf("digit "); // return DIGIT
+        // if (is_blank) printf("blank ");
+        if (is_blank) code = 32; // SPACE return SPACE
+        // if (is_graph) printf("graph ");  
+        // if (is_print) printf("print ");
+        // if (is_punct) printf("punct "); // return PUNCT
+        if (is_cntrl) printf("control ");
+        if (is_space) printf("space ");
+        // if (!is_cntrl && is_space && is_blank) printf("SPACE "); // is_blank is actually enough
+        // if (is_cntrl && is_space) printf("NEWLINE "); // return NEWLINE
+        // if (is_cntrl && !is_space) printf("EOF? "); // END
+        
     }
     return 0;
 }
